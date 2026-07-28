0f691cc51718e9024c3997287765478e32dd86f9
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-07-27 22:45:31 -0500

    Added a main method with tests for the class and verified that it passed them all, fixing any errors along the way

--- src/LinearAlgebra/Vectors/Vector.java
+++ src/LinearAlgebra/Vectors/Vector.java
@@ -45,10 +45,14 @@ public class Vector extends AbstractVector {
      * Gets the normal and returns it
      * @see #normalize()
      * @return Normalized vector
-     * @throws ArithmeticException if it's a 0 length vector due to divide by zero error
+     * @throws ArithmeticException if it's a 0 magnitude vector due to divide by zero error
      */
     public Vector getNormalized() {
         double inverseMag = 1 / getMag();  // We calculate the inverse so normalization can use multiplication instead of repeated division
+
+        if (Double.isInfinite(inverseMag)) {
+            throw new ArithmeticException("Error magnitude is zero resulting in divide by zero");
+        }
 
         Double[] newVectorArray = new Double[elements.length];
 
@@ -57,5 +61,84 @@ public class Vector extends AbstractVector {
         }
 
         return new Vector(newVectorArray);
+    }
+
+    // Testing method
+    public static void main(String[] args) {
+        // Testing Vector construction
+        System.out.println("Creating Vectors");
+        Vector v = new Vector(3, 3.0);
+        Vector v2 = new Vector(4);
+        Vector v3 = new Vector(new Double[] {1d, 2d, 3d, 4d});
+
+        // Testing toString() method
+        System.out.println("Testing toString method");
+        System.out.println(v);
+        System.out.println(v2);
+        System.out.println(v3);
+
+        // Testing grabbing by index
+        System.out.println("Testing get by index method");
+        System.out.println("Grabbing by index for v");
+        System.out.println(v.get(0));
+        System.out.println(v.get(1));
+        System.out.println(v.get(2));
+
+        // Testing Out-Of-Bounds
+        System.out.println("Testing out of bounds error");
+        try {
+            v.get(5);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Testing iterable
+        System.out.println("Testing iteration by printing each vector element");
+        for (Double d : v) {
+            System.out.println(d);
+        }
+
+        // Testing getting the normalized vector
+        System.out.println("Testing the getNormalized method of Vector");
+        System.out.println(v.getNormalized());
+        System.out.println(v3.getNormalized());
+
+        // Test for the divide by zero error caused by zero value magnitude during normalization
+        System.out.println("Testing the getNormalized on a zero magnitude vector");
+        try {
+            System.out.println(v2.getNormalized());
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Test normalizing a vector with destructive call
+        System.out.println("Testing the AbstractVector.normalize method");
+        v.normalize();
+        System.out.println(v);
+
+        // Check to see if the magnitude works
+        System.out.println("Checking magnitude of vectors");
+        System.out.println(v.getMag());   // This one should be 1.0 since it's been normalized
+        System.out.println(v2.getMag());  // This is a zero vector so it should be 0.0
+        System.out.println(v3.getMag());
+
+        // Test alter method
+        System.out.println("Testing alter() method");
+        System.out.println(v);
+        v.alter(0, 5d);
+        System.out.println(v);
+
+        // Test valid dot product
+        System.out.println("Testing dot product");
+        System.out.println(Vector.dotProduct(v2, v3));
+
+        // Test vector length mismatch dot product
+        System.out.println("Testing invalid dot product");
+        try {
+            Vector.dotProduct(v, v3);
+        } catch (VectorLengthMismatch e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
