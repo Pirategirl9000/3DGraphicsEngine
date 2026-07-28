295a341030a8c2c9e083116c924bf01ac765e881
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-07-27 22:08:03 -0500

    Added better error messages and javadocs

--- src/LinearAlgebra/Vectors/Vector.java
+++ src/LinearAlgebra/Vectors/Vector.java
@@ -13,10 +13,10 @@ public class Vector extends AbstractVector {
      * Creates a new Vector with a set size and initial values for all elements
      * @param size The size of the Vector
      * @param initialValue The value to initialize all elements to
-     * @throws IllegalArgumentException If Vector has size less than or equal to 1
+     * @throws InvalidVectorLengthException If Vector has size less than or equal to 1
      */
     public Vector(int size, Double initialValue) {
-        if (size <= 1) throw new IllegalArgumentException("Vectors must have size greater than 1");
+        if (size <= 1) throw new InvalidVectorLengthException("Vectors must have size greater than 1");
 
         this.elements = new Double[size];
         Arrays.fill(elements, initialValue);
@@ -25,7 +25,7 @@ public class Vector extends AbstractVector {
     /**
      * Creates a new Vector with a set size and 0.0 starting values
      * @param size The size of the Vector
-     * @throws IllegalArgumentException If Vector has size less than or equal to 1
+     * @throws InvalidVectorLengthException If Vector has size less than or equal to 1
      */
     public Vector(int size) {
         this(size, 0.0);
@@ -34,10 +34,28 @@ public class Vector extends AbstractVector {
     /**
      * Creates a new Vector from an array of Doubles
      * @param elements The elements for the new Vector
-     * @throws IllegalArgumentException If Vector has size less than or equal to 1
+     * @throws InvalidVectorLengthException If Vector has size less than or equal to 1
      */
     public Vector(Double[] elements) {
-        if (elements.length <= 1) throw new IllegalArgumentException("Vectors must have size greater than 1");
+        if (elements.length <= 1) throw new InvalidVectorLengthException("Vectors must have size greater than 1");
         this.elements = elements;
+    }
+
+    /**
+     * Gets the normal and returns it
+     * @see #normalize()
+     * @return Normalized vector
+     * @throws ArithmeticException if it's a 0 length vector due to divide by zero error
+     */
+    public Vector getNormalized() {
+        double inverseMag = 1 / getMag();  // We calculate the inverse so normalization can use multiplication instead of repeated division
+
+        Double[] newVectorArray = new Double[elements.length];
+
+        for (int i = 0; i < elements.length; i++) {
+            newVectorArray[i] = inverseMag * elements[i];
+        }
+
+        return new Vector(newVectorArray);
     }
 }
