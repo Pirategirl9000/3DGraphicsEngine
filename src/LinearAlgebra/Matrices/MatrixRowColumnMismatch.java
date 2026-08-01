955f9cb7bcfee705675a6bb749d18346198da592
Violet <136397110+Pirategirl9000@users.noreply.github.com>
2026-08-01 01:35:48 -0500

    Added new exception for column row mismatches with matrices

---
+++ src/LinearAlgebra/Matrices/MatrixRowColumnMismatch.java
@@ -0,0 +1,7 @@
+package LinearAlgebra.Matrices;
+
+public class MatrixRowColumnMismatch extends RuntimeException {
+    public MatrixRowColumnMismatch(String message) {
+        super(message);
+    }
+}
